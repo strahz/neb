811cfc7dd58c38af8a8e001afb44783b15ed3fd2
strahz <95108459+strahz@users.noreply.github.com>
2023-08-23 13:54:45 -0700

    Create index.py

---
+++ index.py
@@ -0,0 +1,22 @@
+import pyodbc
+import pandas as pd
+
+db_driver = '{Microsoft Access Driver (*.mdb, *.accdb)}'
+db_path = r"C:\Users\Wesley.Brazas\Music\endurance\Database1.accdb"
+conn_str = (rf'DRIVER={db_driver};'
+            rf'DBQ={db_path};')
+
+conn = pyodbc.connect(conn_str)
+cursor = conn.cursor()
+
+df = pd.read_sql(sql="select * from Documents", con=conn)
+
+cursor.execute("CREATE TABLE QA (name VARCHAR(255), address VARCHAR(255))")
+cursor.commit()
+for i in cursor.tables(tableType='TABLE'):
+    print(i.table_name)
+
+for i in cursor.tables(tableType='VIEW'):
+    print(i.table_name)
+
+conn.close()
